068eefc0267135167b345acb74d5f27c7979e69d
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-08-15 22:21:55 -0400

    changing data with map and returning with ternaries

--- app1/src/ninjas.js
+++ app1/src/ninjas.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const Ninjas = (props) => {
+const Ninjas = (props) => { // or destruct right in the prop param { ninjas }
     const { ninjas } = props
     const ninjaList = ninjas.map(ninja => {
-        return (
+        // if (ninja.age >= 33) {
+        //     return (
+                {/* <div className="ninja" key={ninja.id}>
+                    <div>Name: { ninja.name }</div>
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div>
+                </div> */}
+        //     )
+        // } else {
+        //     return null
+        // }
+        return ninja.age > 33 ? (
             <div className="ninja" key={ninja.id}>
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
-        )
+                    <div>Name: { ninja.name }</div>
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div>
+                </div>
+        ) : null
     })
     return (
         <div className="ninja-list">
