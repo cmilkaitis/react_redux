802ccc54a83eda86d56f17b9c726b6f497456e3a
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-10-10 20:12:11 -0400

    small dated

--- ninja-todo/src/App.js
+++ ninja-todo/src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import ListTodos from './ListTodos';
-import AddTodo from './AddTodo';
+import React, { Component } from "react";
+import ListTodos from "./ListTodos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
-    todos: [
-      {id: 1, content: 'buy milk'},
-      {id: 2, content: 'play game'}
-    ]
-  }
+    todos: [{ id: 1, content: "buy milk" }, { id: 2, content: "play game" }]
+  };
 
-  deleteTodo = (id) => {
+  deleteTodo = id => {
     let todos = this.state.todos.filter(todo => {
       return todo.id !== id;
-    })
+    });
 
     this.setState({
       todos: todos
-    })
-  }
+    });
+  };
 
-  addTodo = (todo) => {
+  addTodo = todo => {
     todo.id = this.state.todos.length + 1;
     let todos = [...this.state.todos, todo];
 
-    this.setState({todos})
-  }
+    this.setState({ todos });
+  };
 
   render() {
     return (
       <div className="container">
-        <h1 className="center blue-text">Todo's</h1>
-        <ListTodos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo}/>
+        <h1 className="center blue-text">Todo's and such</h1>
+        <ListTodos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
